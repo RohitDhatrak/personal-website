f8cfd695ab9b01f60082350d1295d85cc21f03a2
Rohit Dhatrak <rdhatrak19@gmail.com>
2020-11-03 10:46:53 +0530

    fhdfh

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,7 +56,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: ``,
       },
     },
     `gatsby-plugin-feed`,
